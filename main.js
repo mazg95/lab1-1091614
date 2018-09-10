6c96041cffc44b6006c977ba1932fce58baa3ba5
Marco <mazg95@live.com>
2018-09-09 23:33:05 -0600

    se agrega funciones de crear y actualizar

--- main.js
+++ main.js
@@ -1,64 +1,3 @@
-(function(){
-    function addNewSession(){
-        alert('HOLA');
-    }
-
-    function removeSession(){
-
-    }
-
-    function editSession(){
-
-    }
-
-    function load_sessions(){
-        function load_ex(){
-
-        }
-
-    }
-
-    	// handle ajax failure
-	function updateUIFailure() {
-        
-    }
-    
-    function get_insights(){
-        function load_insights(response){
-            Object.keys(response).forEach((k)=> {
-                let pb = document.querySelector(`.${k}`);
-                let lbl = pb.firstChild;
-                goal = response[k]['goal'];
-                actual = response[k]['actual'];
-                pb.setAttribute('aria-valuemax', goal);
-                pb.setAttribute('aria-valuenow', actual);
-                lbl.textContent = `${actual}/${goal} ${k}`;
-
-            });
-
-        } 
-
-        fetch('data/insight.json')
-            .then((r) => {return r.json();})
-            .then((r) => {load_insights(r)})
-            .catch((updateUIFailure()));
-    }
-    
-    function load_suggested_meals(){
-        
-    }
-
-    document.querySelector('.add-session').addEventListener('click', addNewSession, false);
-
-    get_insights();
-
-    load_sessions();
-
-    load_suggested_meals();
-
-    
-})();
-
 var sessions_array = [
     {
         id:1,
@@ -103,6 +42,146 @@ var sessions_array = [
         calorias: 400,
         fc: 140
     }
-]
+];
+
+
+
+
+
+
+
+
+
+
+
+(function(){
+    function addNewSession(){
+        alert('HOLA');
+    }
+
+    function removeSession(){
+
+    }
+
+    function editSession(){
+
+    }
+
+    /*
+    <div class="container">
+  <h2>List Group With Custom Content</h2>
+  <div class="list-group">
+    <a href="#" class="list-group-item active">
+      <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+      <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+    <a href="#" class="list-group-item">
+      <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
+      <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+    <a href="#" class="list-group-item">
+      <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+      <p class="list-group-item-text">List Group Item Text</p>
+    </a>
+  </div>
+</div>
+     */
+
+    function load_sessions(){
+        function load_session(s){
+            let ss = document.createElement('article');
+            let lbl = document.createElement('h3');
+            lbl.textContent = `Session #${s.id}`;
+            let list = document.createElement('div');
+            list.setAttribute('class', 'list-group');
+            let item = document.createElement('a');
+            item.setAttribute('data-toggle', 'modal');
+            item.setAttribute('data-target', '#modal-new-sesion');
+            item.setAttribute('data-id', s.id);
+            item.setAttribute('class', 'list-group-item');
+            let item_data = document.createElement('h4');
+            item_data.setAttribute('class', 'list-group-item-heading')
+            let item_secondary_data = document.createElement('p');
+            item_secondary_data.setAttribute('class', 'list-group-item-text')
+            item_data.textContent = `Calories: ${s.calorias}  / FC: ${s.fc}`;
+            item_secondary_data.textContent =  `Date: ${s.fecha}  /  Duration: ${s.duracion}`;
+            item.appendChild(item_data);
+            item.appendChild(item_secondary_data);
+            list.appendChild(item);
+            ss.appendChild(lbl);
+            ss.appendChild(list);
+            btn = document.createElement('button');
+            btn.textContent = 'Update';
+            btn.setAttribute('class', 'btn btn-default');
+            btn.setAttribute('data-toggle', 'modal');
+            btn.setAttribute('data-target', '#modal-new-sesion');
+            btn.setAttribute('data-id', s.id);
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+                let id = e.target.dataset.id;
+                let obj = sessions_array.find((o)=> {
+                    return o.id == id;
+                });
+                let input = document.querySelector('#id');
+                input.textContent = id;
+                input = document.querySelector("#date_done");
+                input.textContent = obj.fecha;
+                input = document.querySelector("#duration");
+                input.textContent = obj.duracion;
+                input = document.querySelector("#calories");
+                input.textContent = obj.calorias;
+                input = document.querySelector("#fc");
+                input.textContent = obj.fc;
+            }, false);
+            ss.appendChild(btn);
+            container.appendChild(ss);
+        }
+        let container = document.querySelector('div.sessions');
+        container.setAttribute('class', 'sessions');
+        sessions_array.forEach(load_session);
+    }
+
+    	// handle ajax failure
+	function updateUIFailure() {
+        
+    }
+    
+    function get_insights(){
+        function load_insights(response){
+            Object.keys(response).forEach((k)=> {
+                let pb = document.querySelector(`.${k}`);
+                let lbl = pb.firstChild;
+                goal = response[k]['goal'];
+                actual = response[k]['actual'];
+                pb.setAttribute('aria-valuemax', goal);
+                pb.setAttribute('aria-valuenow', actual);
+                lbl.textContent = `${actual}/${goal} ${k}`;
+
+            });
+
+        } 
+
+        fetch('data/insight.json')
+            .then((r) => {return r.json();})
+            .then((r) => {load_insights(r)})
+            .catch((updateUIFailure()));
+    }
+    
+    function load_suggested_meals(){
+        
+    }
+
+    document.querySelector('.add-session').addEventListener('click', addNewSession, false);
+
+    get_insights();
+
+    load_sessions();
+
+    load_suggested_meals();
+
+    
+})();
+
+
 
 
